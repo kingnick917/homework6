4bd031093ad656cfd06c4a403c8c301d8eee67e5
unknown <nmukhtar2001@gmail.com>
2023-02-13 23:09:58 -0800

    made a js

--- Assets/scipt.js
+++ Assets/scipt.js
@@ -1,12 +1,15 @@
-const button = document.querySelector('#button');
-const inputvalue = document.querySelector('#inputvalue');
-const name = document.querySelector('#name');
-const desc = document.querySelector('#desc');
-const tep = document.querySelector('#tep');
+const buttonEL = document.querySelector('#button');
+const inputvalueEL = document.querySelector('#inputvalue');
+const nameEL = document.querySelector('#name');
+const descEL = document.querySelector('#desc');
+const tepEL = document.querySelector('#tep');
+
+buttonEL.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+' &appid=YOU_API_KEY=b059e3b9ba1555004206345a13c9cf24')
+ .then(Response => Response.json())
+ .then(data => console.log(data))
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+' &appid=YOU_API_KEY=b059e3b9ba1555004206345a13c9cf24') 
-.then(Response => Response.json())
-.then(data => console.log(data))
+ .catch(err => alert('wrong city name!'))
 
-.catch(err => alert())
+});
