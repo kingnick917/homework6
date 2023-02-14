171771500fc900e493913e47876ac25dd5e9e248
unknown <nmukhtar2001@gmail.com>
2023-02-14 10:39:17 -0800

    fix a bug in js

--- Assets/scipt.js
+++ Assets/scipt.js
@@ -5,7 +5,7 @@ const descEL = document.querySelector('#desc');
 const tepEL = document.querySelector('#tep');
 
 buttonEL.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+ '&appid=YOU_API_KEY=b059e3b9ba1555004206345a13c9cf24')
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid=YOU_API_KEY=b059e3b9ba1555004206345a13c9cf24')
  .then(Response => Response.json())
  .then(data => console.log(data))
 
