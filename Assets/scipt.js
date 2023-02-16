534335f228bb31dec499621db28a9e6ad3c43c9a
unknown <nmukhtar2001@gmail.com>
2023-02-15 22:28:49 -0800

    made some js changes

--- Assets/scipt.js
+++ Assets/scipt.js
@@ -1,22 +1,26 @@
-const buttonEL = document.querySelector('#button');
-const inputvalueEL = document.querySelector('#inputvalue');
-const nameEL = document.querySelector('#name');
-const descEL = document.querySelector('#desc');
-const tepEL = document.querySelector('#tep');
+const buttonEL = document.querySelector('.button');
+const inputvalueEL = document.querySelector('.inputvalue');
+const nameEL = document.querySelector('.name');
+const descEL = document.querySelector('.desc');
+const tepEL = document.querySelector('.tep');
+
 
 buttonEL.addEventListener('click',function(nameEL){
-  fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&appid=b059e3b9ba1555004206345a13c9cf24')
+   const cityname = inputvalueEL.value;
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityname},CA,US&appid=b059e3b9ba1555004206345a13c9cf24`)
  .then(Response => Response.json())
  .then(data => {
-    const namevalue = data['name'];
-    const tempvalue = data['main']['temp'];
-    const descvalue =data ['weather'][0]['description'];
-
-    nameEL.innerHTML=namevalue;
-    tepEL.innerHTML=tempvalue;
-    descEL.innerHTML=descvalue;
+   //  const namevalue = data['name'];
+   //  const tempvalue = data['main']['temp'];
+   //  const descvalue =data ['weather'][0]['description'];
+    
+   //  nameEL.innerHTML=namevalue;
+   //  tepEL.innerHTML=tempvalue;
+   //  descEL.innerHTML=descvalue;
+   //  input.value ="";
+   console.log(data)
+   const lat =data[0].lat
+   const long = data[0].lon
  })
-
- .catch(err => alert('wrong city name!'))
 
 })
